fda8110fc088f409bb6db3341cac615e4528210b
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-04 13:10:45 +0200

    Finish activity 4 of lesson String and String Operations

--- projects/first/src/main/scala/stringactivity/Activity3.scala
+++ projects/first/src/main/scala/stringactivity/Activity3.scala
@@ -6,13 +6,13 @@ object Activity3 {
   private val quittingString = ":wq"
   //Write a Scala Program to accept strings from the user and concatenate the strings.
   def main(args: Array[String]) : Unit = {
-    val words = ListBuffer.from(args);
+    val words = ListBuffer.from(args)
 
-    var continueToAdd = true;
+    var continueToAdd = true
 
     while(continueToAdd) {
-      println(s"Words currently to concatenate:\n${words.mkString("->")}");
-      val input = scala.io.StdIn.readLine(s"What text would you like to add to it? ('$quittingString' to finish)\n");
+      println(s"Words currently to concatenate:\n${words.mkString("->")}")
+      val input = scala.io.StdIn.readLine(s"What text would you like to add to it? ('$quittingString' to finish)\n")
 
       if(input.equalsIgnoreCase(quittingString)) continueToAdd = false
       else words.addOne(input)
