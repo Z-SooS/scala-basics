16bcf4e14ffefb214766627a2aa7ac294554125a
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-10 14:23:19 +0200

    Finish Activity 3 of lesson

--- projects/RepetitionStatements/src/main/scala/repetition/Repetition.scala
+++ projects/RepetitionStatements/src/main/scala/repetition/Repetition.scala
@@ -1,9 +1,14 @@
 package repetition
 
+import scala.collection.mutable
+
 object Repetition {
   def main(args: Array[String]): Unit = {
     println(reverse(123456))
     println(sumFirstLast(-456))
+    println(digitFrequency(111223))
+    println(digitFrequency(-111223))
+    println(digitFrequency(0))
   }
 
   //Write a Scala program to reverse all the digits of numbers.
@@ -40,5 +45,30 @@ object Repetition {
 
   private def getDigit(currentValue: Int, currentCounter: Int): Byte = {
     (currentValue / (currentCounter / 10)).toByte
+  }
+
+  //Write a Scala program to find the frequency of each digit in each integer.
+  //Hint: Use While Loop
+  private def digitFrequency(n: Int): Map[Int, Int] = {
+    val resultMutableMap = mutable.Map.empty[Int, Int]
+
+    if (n == 0) {
+      resultMutableMap.put(0, 1)
+      return resultMutableMap.toMap
+    }
+    var digitCounter = 10
+    var remainder = if (n > 0) n else n * -1
+
+    while (remainder != 0) {
+      val currentValue = remainder % digitCounter
+      val currentDigit = getDigit(currentValue, digitCounter)
+
+      resultMutableMap.put(currentDigit, resultMutableMap.getOrElse(currentDigit, 0) + 1)
+
+      remainder = remainder - currentValue
+      digitCounter = digitCounter * 10
+    }
+
+    resultMutableMap.toMap
   }
 }
