b7013d68af67a21f2d9b89f916e91a23f2bfa3ce
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-10 14:02:49 +0200

    Finish Activity 2 of lesson

--- projects/RepetitionStatements/src/main/scala/repetition/Repetition.scala
+++ projects/RepetitionStatements/src/main/scala/repetition/Repetition.scala
@@ -3,6 +3,7 @@ package repetition
 object Repetition {
   def main(args: Array[String]): Unit = {
     println(reverse(123456))
+    println(sumFirstLast(-456))
   }
 
   //Write a Scala program to reverse all the digits of numbers.
@@ -16,5 +17,28 @@ object Repetition {
     }
 
     resultString.result().toInt
+  }
+
+  //Write a Scala program to find the sum of the first and last digit of a number
+  //Hint: Use While Loop
+  private def sumFirstLast(n: Int): Byte = {
+    var result = n % 10
+    var digitCounter = 100
+    var remainder = n - result
+
+    while (remainder != 0) {
+      val currentDigitValue = remainder % digitCounter
+
+      if (remainder - currentDigitValue == 0) result += getDigit(currentDigitValue, digitCounter)
+
+      remainder = remainder - currentDigitValue
+      digitCounter = digitCounter * 10
+    }
+
+    result.toByte
+  }
+
+  private def getDigit(currentValue: Int, currentCounter: Int): Byte = {
+    (currentValue / (currentCounter / 10)).toByte
   }
 }
