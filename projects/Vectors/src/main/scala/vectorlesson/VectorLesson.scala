2d4195ca4f5764195c57547fd08db59ec6c582ce
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-11 19:15:34 +0530

    Finish activity 1 for vector lesson

--- projects/Vectors/src/main/scala/vectorlesson/VectorLesson.scala
+++ projects/Vectors/src/main/scala/vectorlesson/VectorLesson.scala
@@ -2,7 +2,18 @@ package vectorlesson
 
 object VectorLesson {
   def main(args: Array[String]): Unit = {
-
+    minMaxVector()
   }
 
+  //Create a Vector with the following numeric items:
+  // 0, 10, 20, 47, -2, 99, -98.
+  // Write a Scala program to find the smallest and the largest numeric item in the Vector.
+  private def minMaxVector(): Unit = {
+    val v = Vector(0, 10, 20, 47, -2, 99, -98)
+
+    // This does not feel in spirit of the lesson
+    // sorted.head?
+    println(s"The smallest item in the vector is ${v.min}")
+    println(s"The largest item in the vector is ${v.max}")
+  }
 }
