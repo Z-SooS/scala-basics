c9fe7b90af2490a05cf4973911764ef7966ca342
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-11 18:33:21 +0530

    Finished activity 3 of tuples lesson

--- projects/Tuples/src/main/scala/tuple/TupleLesson.scala
+++ projects/Tuples/src/main/scala/tuple/TupleLesson.scala
@@ -16,7 +16,10 @@ object TupleLesson {
       ("T-Shirt", "Small", 4.99)
     )
     displayTShirts(items)
+
+    carInventory()
   }
+
 
   //You are free to use whatever data structure is appropriate to store the items mentioned above.
   //The Scala program should then output all "T-Shirt" items in an upper-case format.
@@ -29,5 +32,34 @@ object TupleLesson {
         println(s"${name.toLowerCase()}, ${size.toLowerCase()}, $price")
     }
   }
+
+  //Write a Scala program to simulate an actual car inventory.
+  //In doing so, use a case class to represent a Car type that needs to have a name property of type String and a price property of type Double.
+  //After that, it would help if you defined an aliased type named CarStock that will,
+  //in effect, alias Scala's Tuple2 type and represent a tuple or pair of Car and Int.
+  //The former is the Car mentioned above type, and the Int type means the current stock inventory for a particular Car item.
+  //You should then define a create the following car stock items and use an appropriate data structure from Scala's Collection types.
+  private case class Car(name: String, price: Double)
+  private type CarStock = (Car, Int)
+
+  private def carInventory(): Unit = {
+    val data = getCarStockData
+
+    displayCarList(data)
+  }
+
+  private def displayCarList(list: List[CarStock]):Unit = {
+    for ((car, stock) <- list)
+      println(s"- a ${car.name} with the price of ${car.price} and stock of $stock")
+  }
+
+  private def getCarStockData:List[CarStock] =
+    List(
+      (Car("VW Passat", 10000), 100),
+      (Car("VW Golf", 12000), 50),
+      (Car("BMW 3 series", 20000), 200),
+      (Car("BMW 5 series", 50000), 75),
+      (Car("Mazda 3 series", 15000), 49)
+    )
 
 }
