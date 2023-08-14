b0166e14530d2036d865e2b354fc908e7e9a2edd
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-14 16:41:12 +0530

    Create package for subtasks of exercise

--- projects/CollectionExercise/src/main/scala/exercise/Exercise.scala
+++ projects/CollectionExercise/src/main/scala/exercise/Exercise.scala
@@ -1,5 +1,7 @@
 package exercise
 
+import exercise.subtasks.States
+
 object Exercise {
   def main(args: Array[String]): Unit = {
     States.printStates()
--- projects/CollectionExercise/src/main/scala/exercise/subtasks/States.scala
+++ projects/CollectionExercise/src/main/scala/exercise/subtasks/States.scala
@@ -1,4 +1,4 @@
-package exercise
+package exercise.subtasks
 
 object States {
   private val states = Map(
