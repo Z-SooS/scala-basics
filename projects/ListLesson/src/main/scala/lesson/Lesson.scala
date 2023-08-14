0199042cf6097d5c6edf722d5c53eaa2757503bd
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-14 12:39:30 +0530

    Implement method for activity 1 of lists lesson

--- projects/ListLesson/src/main/scala/lesson/Lesson.scala
+++ projects/ListLesson/src/main/scala/lesson/Lesson.scala
@@ -2,6 +2,12 @@ package lesson
 
 object Lesson {
   def main(args: Array[String]): Unit = {
+    val (list1, list2) = getLists
 
   }
+
+  //Write a Scala program to create two Lists data structures as follows:
+  //The first List will have items named "pencil", "pen", "sharpener", and
+  //The second List will have items name "math book", "french book", "english book".
+  private def getLists:(List[String], List[String]) = (List("pencil", "pen", "sharpener"), List("math book", "french book", "english book"))
 }
