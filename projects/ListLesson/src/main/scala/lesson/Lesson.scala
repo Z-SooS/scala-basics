0f7e0516c6003aae605a016e5ff51e8b8bd88b47
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-14 12:53:25 +0530

    Implement method for activity 2 of lists lesson

--- projects/ListLesson/src/main/scala/lesson/Lesson.scala
+++ projects/ListLesson/src/main/scala/lesson/Lesson.scala
@@ -4,10 +4,21 @@ object Lesson {
   def main(args: Array[String]): Unit = {
     val (list1, list2) = getLists
 
+    val combined = mergeLists(list1,list2)
+
+    outputListForClassroom(combined.flatten)
   }
 
   //Write a Scala program to create two Lists data structures as follows:
   //The first List will have items named "pencil", "pen", "sharpener", and
   //The second List will have items name "math book", "french book", "english book".
   private def getLists:(List[String], List[String]) = (List("pencil", "pen", "sharpener"), List("math book", "french book", "english book"))
+
+  //Merge the two List data structures such that the resulting combined data structure is of type List[List[String]].
+  //Finally, output all items with the literal " is required in the classroom."
+  private def mergeLists[T](lists: List[T]*) : List[List[T]] = lists.toList
+  
+  private def outputListForClassroom(l : List[Any]) : Unit = {
+    l.foreach(printf("%s is required in the classroom.\n", _))
+  }
 }
