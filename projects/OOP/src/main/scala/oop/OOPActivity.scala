499b7990f93b98c13990e7a1b72456cdf2996267
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-15 18:26:53 +0530

    Create Saloon class

--- projects/OOP/src/main/scala/oop/OOPActivity.scala
+++ projects/OOP/src/main/scala/oop/OOPActivity.scala
@@ -1,6 +1,7 @@
 package oop
 
 import oop.subtask.*
+import oop.subtask.beauty.{Customer, DiscountRate, Saloon}
 import oop.subtask.bus.Bus
 
 object OOPActivity extends App{
@@ -11,4 +12,12 @@ object OOPActivity extends App{
   println(chaos.numbers(10,99))
 
   println(Bus("busboy"))
+
+  println(DiscountRate.getServiceDiscountRate("Premium"))
+  println(DiscountRate.getServiceDiscountRate("Gold"))
+  println(DiscountRate.getServiceDiscountRate("Silver"))
+  println(DiscountRate.getServiceDiscountRate(""))
+
+  val saloon = Saloon(chaos)
+  saloon.run()
 }
