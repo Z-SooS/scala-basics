6b1989473eaaa9fad669b38a4ad5d1d6f7a986a9
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-15 19:10:35 +0530

    Finish activity 2 of traits lesson

--- projects/Traits/src/main/scala/traitactivity/TraitsActivity.scala
+++ projects/Traits/src/main/scala/traitactivity/TraitsActivity.scala
@@ -1,10 +1,13 @@
 package traitactivity
 
 import traitactivity.subtask.car.VehicleActivity
+import traitactivity.subtask.pastry.PastryShop
 
 object TraitsActivity {
   def main(args: Array[String]): Unit = {
     VehicleActivity.runActivity()
+
+    PastryShop.runExercise()
   }
-  
+
 }
