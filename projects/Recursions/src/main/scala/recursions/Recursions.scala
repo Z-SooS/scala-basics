92e969d8f09f5eda54d8c9da669307ec224b864d
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-15 10:15:04 +0530

    Add recursive method to calculate fibonacci sequence

--- projects/Recursions/src/main/scala/recursions/Recursions.scala
+++ projects/Recursions/src/main/scala/recursions/Recursions.scala
@@ -1,7 +1,27 @@
 package recursions
 
+import scala.annotation.tailrec
+
 object Recursions {
   def main(args: Array[String]): Unit = {
+    println(fibonacci(90))
+  }
 
+  //Write a program to calculate a Fibonacci sequence recursively using Scala.
+  //Hint: Use the if condition, check the index value and do the increment.
+  def fibonacci(n: Int): List[Long] = {
+    if (n == 0) return List.empty
+    if (n == 1) return List(0)
+
+    fibonacci(n, List(0, 1))
+  }
+
+  @tailrec
+  private def fibonacci(n: Int, numbers: List[Long]): List[Long] = {
+    if (n <= numbers.length) return numbers
+
+    val lastTwo = numbers.takeRight(2)
+
+    fibonacci(n, numbers :+ lastTwo.sum)
   }
 }
