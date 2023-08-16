42fdbd4b11f205e3b94242ac5c2567ac945f17a5
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-16 15:07:06 +0530

    Initialize project for IO activities, also include trial code

---
+++ projects/IOHandling/src/main/scala/iohandler/Main.scala
@@ -0,0 +1,31 @@
+package iohandler
+
+import com.opencsv.CSVReader
+
+import java.io.InputStreamReader
+
+object Main {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  private def messingAround():Unit = {
+    val filename = "/trial.csv"
+    val csvStream = getClass.getResourceAsStream(filename)
+
+    val json = ujson.Arr(ujson.Obj("id" -> 1, "name" -> "jason"))
+    println(ujson.write(json, indent = 4))
+
+    val reader = new CSVReader(new InputStreamReader(csvStream, "UTF-8"))
+    try {
+      val iter = reader.iterator()
+      while (iter.hasNext) {
+        val Array(id, firstName, lastName, email) = iter.next()
+        println(s"$id|$firstName $lastName|$email")
+      }
+    } finally {
+      reader.close()
+    }
+  }
+}
+
