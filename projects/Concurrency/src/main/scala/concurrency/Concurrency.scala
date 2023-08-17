b9e0fcb30e81c4edea2f3c81816ad3fb1c7ba73f
Zoltán Soós <zoli.soos98@gmail.com>
2023-08-17 11:49:17 +0530

    Create class that extends Thread

--- projects/Concurrency/src/main/scala/concurrency/Concurrency.scala
+++ projects/Concurrency/src/main/scala/concurrency/Concurrency.scala
@@ -2,6 +2,21 @@ package concurrency
 
 object Concurrency {
   def main(args: Array[String]): Unit = {
+    val t1 = new MyThread()
+    t1.setPriority(10)
+    t1.setName("Thread One")
+    val t2 = new MyThread()
+    t2.setPriority(1)
+    t2.setName("Thread Two")
+    t1.start()
+    t2.start()
+  }
 
+  class MyThread extends Thread {
+    override def run(): Unit = {
+      for (i <- 1 to 10) {
+        println(s"Hello this is thread ${this.getName} iteration $i")
+      }
+    }
   }
 }
